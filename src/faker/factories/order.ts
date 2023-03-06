770359b3540763e7b104d2aaec28071b7191a133
Lucas Pinto <github@lucks.pt>
2023-03-06 23:30:56 +0000

    fix: sometimes carrier is undefined, so let's force all to exist

--- src/faker/factories/order.ts
+++ src/faker/factories/order.ts
@@ -13,15 +13,17 @@ export const generateRandomOrder = (consumer: Consumer): Order => {
 	const itemQuantity = faker.datatype.number(10);
 	for (let i = 0; i < itemQuantity; i++) {
 		const producer = faker.helpers.arrayElement(fakerContainer.randomProducers);
+		if (!producer) continue;
 		const productionUnit = faker.helpers.arrayElement(producer.productionUnits.getItems());
+		if (!productionUnit) continue;
 		const producerProduct = faker.helpers.arrayElement(productionUnit.products.getItems());
-		if (producerProduct) {
-			const carrier = faker.helpers.arrayElement(productionUnit.carriers.getItems());
-			const shipment = faker.helpers.arrayElement(carrier.shipments.getItems());
-			if (shipment) {
-				order.items.add(generateRandomOrderItem(order, producerProduct, shipment));
-			}
-		}
+		if (!producerProduct) continue;
+		const carrier = faker.helpers.arrayElement(productionUnit.carriers.getItems());
+		if (!carrier) continue;
+		const shipment = faker.helpers.arrayElement(carrier.shipments.getItems());
+		if (!shipment) continue;
+
+		order.items.add(generateRandomOrderItem(order, producerProduct, shipment));
 	}
 
 	order.shippingAddress = generateRandomAddress();
