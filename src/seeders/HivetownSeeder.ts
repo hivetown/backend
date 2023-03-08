394d6325e8a9d30888c795d699038fba9d7f47b3
Lucas Pinto <github@lucks.pt>
2023-03-08 00:21:18 +0000

    chore: working seeder for ProductSpec and its dependents

--- src/seeders/HivetownSeeder.ts
+++ src/seeders/HivetownSeeder.ts
@@ -1,61 +1,20 @@
 import type { EntityManager } from '@mikro-orm/core';
-import { Seeder } from '@mikro-orm/seeder';
-import { AddressFactory } from './factories/Address';
+import { faker, Seeder } from '@mikro-orm/seeder';
 import { ProductSpecFactory } from './factories/ProductSpec';
 import { ProductSpecCategoryFactory } from './factories/ProductSpecCategory';
 import { CategoryFactory } from './factories/Category';
 import { FieldFactory } from './factories/Field';
-import { FieldPossibleValue } from '../entities';
 import { FieldType } from '../enums';
 import { ProductSpecFieldFactory } from './factories/ProductSpecField';
 import { FieldPossibleValueFactory } from './factories/FieldPossibleValue';
 
 export class HivetownSeeder extends Seeder {
 	public async run(em: EntityManager): Promise<void> {
-		// const productSpecs = new ProductSpecFactory(em).each((spec) => {
-		// 	spec.categories.set(
-		// 		new ProductSpecCategoryFactory(em)
-		// 			.each((specCat) => {
-		// 				const field = new FieldFactory(em)
-		// 					.each((field) => {
-		// 						const amount = Math.floor(Math.random() * 5);
-		// 						if (amount !== 0) {
-		// 							field.type = FieldType.Enum;
-		// 							for (let i = 0; i < amount; i++) {
-		// 								field.possibleValues.add(new FieldPossibleValueFactory(em).makeOne());
-		// 							}
-		// 						}
-		// 					})
-		// 					.makeOne();
-
-		// 				specCat.category = new CategoryFactory(em)
-		// 					.each((category) => {
-		// 						category.fields.add(field);
-		// 					})
-		// 					.makeOne();
-
-		// 				specCat.fields.set(
-		// 					new ProductSpecFieldFactory(em)
-		// 						.each((specField) => {
-		// 							specField.field = field;
-		// 						})
-		// 						.make(5)
-		// 				);
-		// 			})
-		// 			.make(5)
-		// 	);
-		// });
-
-		// await productSpecs.create(10);
-
-		// We create a ProductSpec
-		await new ProductSpecFactory(em)
+		const productSpecs = await new ProductSpecFactory(em)
 			.each((spec) => {
-				// Each ProductSpec has ProductSpecCategories
 				spec.categories.set(
 					new ProductSpecCategoryFactory(em)
 						.each((specCategory) => {
-							// Each ProductSpecCategory has a link to a Category and ProductSpecFields of that Category
 							specCategory.category = new CategoryFactory(em)
 								.each((category) => {
 									// We make some Fields for the Category
@@ -65,8 +24,8 @@ export class HivetownSeeder extends Seeder {
 									category.fields.add(
 										new FieldFactory(em)
 											.each((field) => {
-												const amount = Math.floor(Math.random() * 5);
-												if (amount !== 0) {
+												const amount = faker.datatype.number(5);
+												if (amount) {
 													field.type = FieldType.Enum;
 													field.possibleValues.set(new FieldPossibleValueFactory(em).make(amount));
 												}
@@ -76,19 +35,31 @@ export class HivetownSeeder extends Seeder {
 								})
 								.makeOne();
 
-							// TODO: THIS THROWS AN EXCEPTION (TypeError: Cannot read properties of undefined (reading '__helper') when console.logging instead of setting)
-							// We link the ProductSpecFields to the Fields of the Category
-							const productSpecFactory = new ProductSpecFieldFactory(em);
-							// specCategory.fields.set(specCategory.category.fields.getItems().map((field) => productSpecFactory.makeOne({ field })));
-							productSpecFactory.makeOne({
-								field: { id: 1 }
-							});
-							console.log(specCategory.category.fields.getItems().map((field) => field.name));
+							return specCategory;
 						})
-						.make(5)
+						.make(10)
 				);
 			})
 			.create(10);
-		// await em.persistAndFlush(productSpecs);
+
+		// Create ProductSpecFields. THIS IS THE CORRECT WAY TO DO IT BECAUSE IT DEPENDS ON IDS CREATED BEFOREHAND
+		const productSpecFieldFactory = new ProductSpecFieldFactory(em);
+		productSpecs.forEach((spec) => {
+			spec.categories.getItems().forEach((specCategory) => {
+				specCategory.category.fields.getItems().map((field) =>
+					productSpecFieldFactory.makeOne({
+						field: field.id,
+						productSpecCategory: { category: specCategory.category.id, productSpec: spec.id }
+					})
+				);
+			});
+		});
+
+		// Flush the changes to the database
+		await em.flush();
 	}
 }
+
+// if (field.type === FieldType.Enum) {
+// 	productSpecField.value = faker.random.arrayElement(field.possibleValues.getItems());
+// }
