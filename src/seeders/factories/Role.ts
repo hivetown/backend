e49e540e710c7dc5a217ca8b544cc70e6003e8a5
Lucas Pinto <github@lucks.pt>
2023-05-02 19:16:24 +0100

    feat: example roles

---
+++ src/seeders/factories/Role.ts
@@ -0,0 +1,24 @@
+import type { EntityManager } from '@mikro-orm/knex';
+import { Role } from '../../entities/Role';
+import { Permission } from '../../enums/Permission';
+
+const baseRole = [
+	{
+		name: 'Root',
+		permissions: Permission.ALL
+	},
+	{
+		name: 'ContentManager',
+		permissions: Permission.ALL_CATEGORY | Permission.ALL_PRODUCT
+	},
+	{
+		name: 'Editor',
+		permissions: Permission.WRITE_CATEGORY | Permission.WRITE_PRODUCT
+	},
+	{
+		name: 'AccountManager',
+		permissions: Permission.READ_OTHER_CONSUMER | Permission.WRITE_OTHER_CONSUMER
+	}
+] as Omit<Role, 'id'>[];
+
+export const createRoles = (em: EntityManager) => baseRole.map((role) => em.create(Role, role));
