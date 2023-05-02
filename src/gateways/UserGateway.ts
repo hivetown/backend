fe15c69d347392c9004ba6e4509a8ec268de659e
Lucas Pinto <github@lucks.pt>
2023-05-02 19:04:58 +0100

    feat: authorization

--- src/gateways/UserGateway.ts
+++ src/gateways/UserGateway.ts
@@ -1,5 +1,6 @@
 import type { EntityRepository, MikroORM } from '@mikro-orm/mysql';
 import { User } from '../entities';
+import type { UserOptions } from '../interfaces/UserOptions';
 
 export class UserGateway {
 	private repository: EntityRepository<User>;
@@ -14,8 +15,8 @@ export class UserGateway {
 		return user;
 	}
 
-	public async findByAuthId(authId: string): Promise<User | null> {
-		return this.repository.findOne({ authId });
+	public async findByAuthId(authId: string, options?: UserOptions): Promise<User | null> {
+		return this.repository.findOne({ authId }, { populate: options?.populate as any });
 	}
 
 	public async findById(id: number): Promise<User | null> {
--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@decorators/di';
-import type { Middleware } from '@decorators/express';
 import type { NextFunction, Request, Response } from 'express';
+import { container } from '..';
+import { UnauthorizedError } from '../errors/UnauthorizedError';
+import type { User } from '../entities';
+import { ForbiddenError } from '../errors/ForbiddenError';
 
-@Injectable()
-export class AuthorizationMiddleware implements Middleware {
-	public async use(req: Request, _res: Response, next: NextFunction) {
-		await console.log();
+/**
+ * A generic validation function for the authorization middleware
+ * @param user The user to validate
+ * @throws ForbiddenError if the user is not valid
+ * @returns void
+ */
+type AuthorizationValidation = (user: User) => void;
+
+export const authorizationMiddleware = ({
+	permissions,
+	otherValidations
+}: {
+	permissions?: number;
+	otherValidations?: AuthorizationValidation[];
+}) => {
+	return async (req: Request, _res: Response, next: NextFunction) => {
+		if (!req.authUser) throw new UnauthorizedError('User is not authenticated');
+
+		const user = await container.userGateway.findByAuthId(req.authUser.uid, { populate: ['role'] });
+		if (!user) throw new UnauthorizedError('User is not authenticated');
+
+		// Bitwise AND operator to check if the user has the required permission(s)
+		if (permissions) {
+			const hasBasePermission = (user.role.permissions & permissions) === permissions;
+			if (!hasBasePermission)
+				throw new ForbiddenError('User does not have enough permissions', {
+					details: { user: user.role.permissions, required: permissions }
+				});
+		}
+
+		if (otherValidations?.length) {
+			for (const validation of otherValidations) {
+				// validation throws an error if the user is not valid
+				validation(user);
+			}
+		}
+
 		next();
-	}
-}
+	};
+};
