8016a105b98b8603586f9518f45dfbeadaab5039
Romulo <romulo3218@gmail.com>
2023-05-19 19:37:43 +0100

    feat: dados para relatorio do consumidor produzidos

--- src/utils/handleReportProducts.ts
+++ src/utils/handleReportProducts.ts
@@ -1,31 +1,32 @@
 import type { OrderItem } from '../entities';
 
 export function handleReportProducts(atual: any[], orderItem: OrderItem, opcao: string) {
-	// console.log('orderItems', orderItem);
-	// console.log('opcao', opcao);
-	console.log('atual', atual);
+	const obj = atual.find((obj) => Number(obj.id) === Number(orderItem.producerProduct.id));
 
-	// para o numeroEncomendas
-	// const encomendas: any[] = [];
-	// const totalProdutos: any[] = [];
-	// const comprasTotais: any[] = [];
-	// const produtosEncomendados: any[] = [];
-
-	if (opcao === 'numeroEncomendas') {
-		const obj = atual.find((obj) => obj.id === orderItem.producerProduct.id);
-		if (obj) {
-			console.log(obj);
+	if (obj) {
+		if (opcao === 'numeroEncomendas') {
+			console.log('obj antes', obj);
 			obj.numeroEncomendas++;
-		} else {
-			atual.push({
-				id: orderItem.producerProduct.id,
-				nome: orderItem.producerProduct.productSpec.name,
-				numeroEncomendas: 1
-			});
+			console.log('obj depois', obj);
+		} else if (opcao === 'totalProdutos') {
+			console.log('obj antes', obj);
+			obj.totalProdutos += orderItem.quantity;
+			console.log('obj depois', obj);
+		} else if (opcao === 'comprasTotais') {
+			console.log('obj antes', obj);
+			obj.comprasTotais += orderItem.quantity * orderItem.price;
+			console.log('obj depois', obj);
 		}
+	} else if (opcao === 'numeroEncomendas') {
+		atual.push({ id: orderItem.producerProduct.id, nome: orderItem.producerProduct.productSpec.name, numeroEncomendas: 1 });
 	} else if (opcao === 'totalProdutos') {
+		atual.push({ id: orderItem.producerProduct.id, nome: orderItem.producerProduct.productSpec.name, totalProdutos: orderItem.quantity });
 	} else if (opcao === 'comprasTotais') {
-	} else if (opcao === 'numeroProdutosEncomendados') {
+		atual.push({
+			id: orderItem.producerProduct.id,
+			nome: orderItem.producerProduct.productSpec.name,
+			comprasTotais: orderItem.quantity * orderItem.price
+		});
 	}
 
 	return atual;
